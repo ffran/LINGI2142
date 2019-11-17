68699d049619790964b87569efb29d21adc28cfa
root <root@group9>
2019-11-17 15:29:16 +0000

    mako bgp

--- DPTests.sh
+++ DPTests.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# My first script
 
-sudo ip netns exec P1 ping6 fde4:9::1111 -c5 
-sudo ip netns exec P3 ping6 fde4:9::1111 -c5
+adress=(fde4:9::1111)
+routers=("P1" "P2" "P3")
+#sudo ip netns exec P1 ping6 fde4:9::1111 -c5 
+#sudo ip netns exec P3 ping6 fde4:9::1111 -c5
+for t in ${adress[@]}; do
+  sudo ip netns exec P1 ping6 fde4:9::1111 -c5 $t
+done
 #sudo ./connect_to.sh automatetest_cfg P1 && 
 #ping6 fde4:9::1111 -c5  > /dev/null
 if [ $? -eq 0 ]
