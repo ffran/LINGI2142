fcd94f3c16bd73388916d13f9869b17d0665614b
root <root@group9>
2019-11-17 17:48:41 +0000

    script working

--- make_all_bgp.sh
+++ make_all_bgp.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 FILES=json_bgp/*
+
+
+
 for f in $FILES.*
 do
-  echo "Processing ${f%%.*} file..."
+
+ echo ${f##*/}
+a=${f##*/}
+ echo ${a%.txt}
+
+a= echo ${a%.*}
+
+  echo "Processing $a file..."
+
+   initial="$(echo ${a%.*} | head -c 2 | tail -c 1)"
+  echo "$initial"
   # take action on each file. $f store current file name
-  sudo ./make_bgp_conf.py -t bgp.mako -i $f -o ${f%%.*}.conf    
-  
+  sudo ./make_bgp_conf.py -t bgp.mako -i $f -o automatetest_cfg/${a%.*}/"p$initial"_bgp_test.conf    
+
 done
