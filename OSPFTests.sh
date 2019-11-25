883a7bdff99d9a7f97e53aff2964423dcd1c9f91
root <root@group9>
2019-11-25 09:17:23 +0000

    backup

--- OSPFTests.sh
+++ OSPFTests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-adress=(fde4:9::1111 fde4:9::22 fde4:9::33)
-routers=(P11 P1 P2 P3)
-sudo ip netns exec $1 sudo ip link set  down
+adress=(fde4:9::1111 fde4:9::22 fde4:9::33 fde4:9::11 fde4:9::33 fde4:9::44 fde4:9::55 fde4:9::99 fde4:9:1111::9191 fde4:9:1111::9292 fde4:9::1111 fde4:9::1010)
+routers=(P1 P2 P3 P4 P5 P9 P91 P92 P10 P11)
 for r in ${routers[@]};
 do
 for t in ${adress[@]}; 
@@ -13,6 +12,7 @@ echo "Traceroute6 result from ${r} to ${t} :" >> Rapport_Test_OSPF.txt
 sudo ip netns exec $r traceroute6 $t >> Rapport_Test_OSPF.txt
 else
 echo "Valid connection from ${r} to ${t}" >> Rapport_Test_OSPF.txt
+echo "Valid connection from ${r} to ${t}"
 fi  
 done
 done
--- SecurityTests.sh
+++ SecurityTests.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+adress=(fde4:9::33)
+routers=(P1)
 for r in ${routers[@]};
 do
 for t in ${adress[@]}; 
 do
-sudo ip netns exec $r ping6 $t -c25 >> Rapport_Securite_Test
+sudo ip netns exec $r ping6 $t -c25 > /dev/null
+if [ "${?}" -ne 0 ]; then
+echo "Firewall contre le flooding fonctionnel ! Attaque maitrisée ! ${r} to ${t}" >> Rapport_Securite_Test
+else
+echo "Firewall non fonctionnel, attaque non contrée ..." >> Rapport_Securite_Test
+fi   
 done
 done
 
