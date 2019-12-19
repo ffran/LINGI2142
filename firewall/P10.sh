4b2ef2b7d56a4960a5f29c40896d7867cfa8fa3b
root <root@group9>
2019-12-19 18:03:51 +0000

    firewall config

--- firewall/P10.sh
+++ firewall/P10.sh
@@ -24,19 +24,16 @@ ip6tables -A OUTPUT -p ipv6-icmp -j ACCEPT
 
 #allow OSPF (number protocol = 89)
 ip6tables -A INPUT -p 89 -j ACCEPT
-ip6tables -A OUTPUT -p 89 -j ACCEPT
 ip6tables -A FORWARD -p 89 -j ACCEPT
 
 #allow traceroute6
-ip6tables -I INPUT -p udp --dport 33434:33474 -j ACCEPT
+ip6tables -I INPUT -p udp --dport 33434:33474 -j ACCEPT --match limit --limit  20/minute
 ip6tables -I FORWARD -p udp --dport 33434:33474 -j ACCEPT
 ip6tables -I OUTPUT -p udp --dport 33434:33474 -j ACCEPT
 
 #allow BGP
 ip6tables -A INPUT -p tcp --dport 179 -j ACCEPT
 ip6tables -A INPUT -p tcp --sport 179 -j ACCEPT
-ip6tables -A FORWARD -p tcp --dport 179 -j ACCEPT
-ip6tables -A FORWARD -p tcp --sport 179 -j ACCEPT
 ip6tables -A OUTPUT -p tcp --dport 179 -j ACCEPT
 ip6tables -A OUTPUT -p tcp --sport 179 -j ACCEPT
 
