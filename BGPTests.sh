134b3ab9f3576f6eb2222a0f1733c892810c57db
root <root@group9>
2019-12-20 09:36:11 +0000

    clean root

--- BGPTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-adress=(fde4:9::1111 fde4:9::22 fde4:9::33)  
-routers=("P11 P1 P2")  
-for r in ${routers[@]}; do
-  sudo ./connect_to.sh ./automatetest_cfg/ $r << 'EOF'
-  LD_LIBRARY_PATH=/usr/local/lib vtysh
-  show bgp summary
-EOF
